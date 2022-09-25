89d32e05f83a3e50070d31a1af3e079e667de628
benson yang <f932037@gmail.com>
2022-09-25 11:30:51 +0800

    init project

---
+++ src/main/java/com/benson/railwaytest/RailwaytestApplication.java
@@ -0,0 +1,13 @@
+package com.benson.railwaytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwaytestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwaytestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benson/railwaytest/RailwaytestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benson.railwaytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwaytestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
